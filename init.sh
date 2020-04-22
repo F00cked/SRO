6b3d7a00c75467522f156f4b42e22bbdd1f81b6a
F00cked <blacktha@gmail.com>
2020-04-22 15:33:57 +0800

    add config

---
+++ init.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+EXEC="/usr/bin/sss"
+CONF="/usr/bin/config.json"
+# reuse existing config when the container restarts
+run() {
+  if [ -f ${CONF} ]; then
+    echo "Found existing config..."
+  else
+    if [ -z ${PSK} ]; then
+      PSK=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 32)
+      echo "Using PSK: ${PSK}"
+    else
+      tee -a ${CONF} << EOF
+{
+    "server":"0.0.0.0",
+    "server_port":${PORT},
+    "password":"${PSK}",
+    "method":"${$METHOD}",
+    "timeout":300,
+    "fast_open":true,
+    "plugin":"obfs-server",
+    "plugin-opts":"obfs=http"
+}
+EOF
+    fi
+      ${EXEC} -c ${CONF}
+  fi
+}
+if [ -z "$@" ]; then
+  run
+else
+  exec "$@"
+fi
